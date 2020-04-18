4b73a84e9878cd0bab4a0808b658ab37a0f8713d
kazivana <kazivana@gmail.com>
2020-04-18 15:21:39 +0200

    Interactive plot

--- code/04_plot.R
+++ code/04_plot.R
@@ -3,28 +3,30 @@ library(ggplot2)
 library(gganimate)
 library(data.table)
 
-item_info <- readRDS('./data/item_info.rds')
-summary <- readRDS('./data/summary.rds')
-mean_summary <- readRDS('./data/mean_summary.rds')
+mean_summary_pm <- readRDS('./data/mean_summary_pm.rds')
+mean_summary_pol <- readRDS('./data/mean_summary_pol.rds')
+mean_summary_pm
+mean_summary_pol
 
-head(item_info)
-head(summary)
 
-summary_short <- summary[, (3:5) := NULL]
-summary_short[, year := year(as.Date(`Measurement date`, "%Y-%m-%d"))]
-summary_short[, (1:2) := NULL]
-summary_short
 
-summary_tidy <- melt(summary_short, id.vars = 'year')
-summary_tidy
-summary_pm <- summary_short[5:7]
-summary_pm_tidy <- melt(summary_pm, id.vars = 'year')
-summary_pm_tidy
+mean_summary_pm_tidy <- melt(mean_summary_pm, id.vars = 'year')
+mean_summary_pm_tidy
 
-mean_summary_tidy <- melt(mean_summary, id.vars = 'year')
-mean_summary_tidy
+mean_summary_pol_tidy <- melt(mean_summary_pol, id.vars = 'year')
+mean_summary_pol_tidy
 
-ggplot(mean_summary_tidy, aes(x = variable, y = value, fill = variable)) +
+ggplot(mean_summary_pm_tidy, aes(x = variable, y = value, fill = variable)) +
+  geom_bar(stat='identity') +
+  theme_bw() +
+  transition_states(
+    year,
+    transition_length = 2,
+    state_length = 1
+  ) +
+  ease_aes('sine-in-out')
+
+ggplot(mean_summary_pol_tidy, aes(x = variable, y = value, fill = variable)) +
   geom_bar(stat='identity') +
   theme_bw() +
   transition_states(
