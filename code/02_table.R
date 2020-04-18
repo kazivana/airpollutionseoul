fbdf808889536a00dbf5df3c9c8f29819e67726b
kazivana <kazivana@gmail.com>
2020-04-18 15:15:54 +0200

    Tables

--- code/02_table.R
+++ code/02_table.R
@@ -13,3 +13,20 @@ mean_summary <- summary[, .(mean_s02 = mean(SO2),
                         by = year(as.Date(summary$`Measurement date`, "%Y-%m-%d"))]
 mean_summary
 
+mean_summary_pol <- summary[, .(mean_sO2 = mean(SO2),
+                                mean_no2 = mean(NO2),
+                                mean_o3 = mean(O3),
+                                mean_co = mean(CO)),
+                            by = year(as.Date(summary$`Measurement date`, "%Y-%m-%d"))]
+
+mean_summary_pol
+
+mean_summary_pm <- summary[, .(mean_pm10 = mean(PM10),
+                               mean_pm25 = mean(PM2.5)),
+                           by = year(as.Date(summary$`Measurement date`, "%Y-%m-%d"))]
+
+mean_summary_pm
+
+saveRDS(mean_summary, './data/mean_summary.rds')
+saveRDS(mean_summary_pol, './data/mean_summary_pol.rds')
+saveRDS(mean_summary_pm, './data/mean_summary_pm.rds')
