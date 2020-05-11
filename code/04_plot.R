7bfe8d939ebe5fbe72734f447da51c468470c328
kazivana <kazivana@gmail.com>
2020-05-11 08:42:16 +0200

    Interactive plot

--- code/04_plot.R
+++ code/04_plot.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(gganimate)
 library(data.table)
+library(plotly)
 
 mean_summary_pm <- readRDS('./data/mean_summary_pm.rds')
 mean_summary_pol <- readRDS('./data/mean_summary_pol.rds')
@@ -13,8 +14,24 @@ mean_summary_pol
 mean_summary_pm_tidy <- melt(mean_summary_pm, id.vars = 'year')
 mean_summary_pm_tidy
 
+p <- mean_summary_pm_tidy %>%
+  ggplot(aes(year, value, size = value, color = variable)) +
+  geom_point() +
+  theme_bw()
+
+ggplotly(p)
+
+
 mean_summary_pol_tidy <- melt(mean_summary_pol, id.vars = 'year')
 mean_summary_pol_tidy
+
+w <- mean_summary_pol_tidy %>%
+  ggplot(aes(year, value, size = value, color = variable)) +
+  geom_point() +
+  theme_bw()
+
+ggplotly(p)
+
 
 ggplot(mean_summary_pm_tidy, aes(x = variable, y = value, fill = variable)) +
   geom_bar(stat='identity') +
@@ -41,3 +58,5 @@ getwd()
 
 
 anim_save('pollutant_values.gif')
+
+
