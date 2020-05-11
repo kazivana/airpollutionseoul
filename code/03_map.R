b24f3a6dc025c23340a4be8439ec28e14aa9d5dd
kazivana <kazivana@gmail.com>
2020-05-11 08:40:12 +0200

    Leaflet commit

--- code/03_map.R
+++ code/03_map.R
@@ -1,11 +1,21 @@
 station_info <- readRDS('./data/station_info.rds')
-station_info
+station_info_coords <- station_info[4:5]
+station_info_coords
+
 
 library(mapview)
 library(sf)
+library(leaflet)
 
-station_coords <- st_as_sf(station_info,
-                           coords = c('Latitude', 'Longitude'))
+station_coords <- st_as_sf(station_info_coords,
+                           coords = c('Latitude', 'Longitude'),
+                           crs = 5181)
                            
-mapview(station_coords, map.types = 'Stamen.TerrainBackground')
+mapview(station_coords, map.types = 'OpenTopoMap')
 
+sessionInfo()
+
+themap <- leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng=station_info_coords$Longitude, lat = station_info_coords$Latitude)
+themap
